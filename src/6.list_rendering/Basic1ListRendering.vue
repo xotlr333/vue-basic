6fb67e02a85a84a4cc93d5a944104156b6135c6a
teasik <xotlr333@naver.com>
2023-05-09 14:59:58 +0900

    list rendering

---
+++ src/6.list_rendering/Basic1ListRendering.vue
@@ -0,0 +1,33 @@
+<template>
+	<li v-for="(item, index) in itemArray" :key="item.id">
+        {{ item }}
+        <p>{{ item.message }}</p>
+        <p>{{ index }}</p>
+        <p>{{ parentMessage }}</p>
+        <!-- <p>{{ item.id }}</p> -->
+    </li>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+    const parentMessage = ref('Parent');
+
+    const itemArray = [
+        { id: 'i1', message: 'Foo' },
+        { id: 'i2', message: 'Soo' },
+        { id: 'i3', message: 'Tom' },
+        { id: 'i4', message: 'Bar' }
+    ]
+
+</script>
+
+<style scoped>
+li {
+    margin: 1rem;
+    padding: 0.75rem;
+    border: 2px dashed blue;
+    list-style: none;
+    font-size: 1.5rem;
+}
+</style>
